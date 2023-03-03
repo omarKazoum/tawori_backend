829e58d07f435cbe5a83c012fd8469f23ff4ef92
omarkazoum <omarkazoum96@gmail.com>
2023-03-03 09:09:47 +0100

    created backend project

---
+++ src/main/java/com/tawori/spaceManagment/TaworiApplication.java
@@ -0,0 +1,13 @@
+package com.tawori.spaceManagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaworiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaworiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tawori/spaceManagment/FileRouge2TaworiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tawori.spaceManagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileRouge2TaworiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
